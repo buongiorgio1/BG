4255137f2db57fb06485328906fa315573043dcb
buongiorgio1 <33639023+buongiorgio1@users.noreply.github.com>
2018-05-07 22:27:03 -0400

    Add files via upload

--- js/p2.js
+++ js/p2.js
@@ -15,7 +15,7 @@
      	for (i = 0; i < dots.length; i++) {
      		dots[i].className = dots[i].className.replace(" active", "");
      	}
-     	slides[slideIndex - 1].style.display = "block";
+     	slides[slideIndex-1].style.display = "block";
      	dots[slideIndex - 1].className += " active";
      	setTimeout(showSlides, 5000); // Change image every 4 seconds
      }
@@ -24,12 +24,21 @@
      myButton.addEventListener('click', doSomething, false);
 
      function doSomething() {
-     	myText.textContent = "Welcome to rapid language learning methods!!!";
+     	// 	myText.textContent = "Welcome to rapid language learning methods!!!";
      	//myText.onclick= "Welcome";
-     	//<img src="https://images.unsplash.com/photo-1423655156442-ccc11daa4e99?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&s=bf44ec3af7e7faf6936af0144c40d991" alt="New York" style="width:100%">
      }
-     
-     function run(){
-  var newName = document.getElementById("name").value;
-  document.getElementById("change").innerHTML= newName;
-}
+
+     function run() {
+     	var newName = document.getElementById("name").value;
+     	document.getElementById("change").innerHTML = newName;
+     	document.getElementById('here').style.display = 'block';
+     }
+
+     function signUpEs() {
+     	// window.open(../courses.html);
+     	document.getElementById('id01').style.display = 'block';
+     }
+
+     function signUpIt() {
+     	document.getElementById('id02').style.display = 'block';
+     }
